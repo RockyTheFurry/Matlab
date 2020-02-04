99d7b1ffc0583a77b2433d74d9a87603cb39575b
RockyTheFurry <55091378+RockyTheFurry@users.noreply.github.com>
2020-02-04 15:57:37 +0200

    lesson 2

---
+++ lab1_diary.m
@@ -0,0 +1,353 @@
+
+Vm = [-1 0.3 1.5 2.5 3.2];
+im = [1.1 2.2 2.1 3.2 4.7]*le -3;
+{Error using <a href="matlab:matlab.internal.language.introspective.errorDocCallback('le')" style="font-weight:bold"> <= </a>
+Not enough input arguments.
+} 
+plot(Vm,Im,'o-')
+{Undefined function or variable 'Im'.
+} 
+im = [1.1 2.2 2.1 3.2 4.7]*le -3;
+{Error using <a href="matlab:matlab.internal.language.introspective.errorDocCallback('le')" style="font-weight:bold"> <= </a>
+Not enough input arguments.
+} 
+Im = [1.1 2.2 2.1 3.2 4.7]*le -3;
+{Error using <a href="matlab:matlab.internal.language.introspective.errorDocCallback('le')" style="font-weight:bold"> <= </a>
+Not enough input arguments.
+} 
+plot(Vm,im,'o-')
+{Undefined function or variable 'im'.
+} 
+plot(Vm, im,'o-')
+{Undefined function or variable 'im'.
+} 
+plot(Vm, sim,'o-')%aprakstisim tos datus ar 
+{Command syntax error see SIM command for details
+} 
+%2.kartas polinomu
+%y = C1*x^2+C2*x+C3
+%to koeficentus atradis funkcija
+%polifyt - polynomial fitting 
+%sitakse
+%C = polyfit(x,y,n)
+%N-polinoma karta
+C = polyfit(Vm,Im,2)
+{Undefined function or variable 'Im'.
+} 
+C = polyfit(Vm,sim,2)
+{Command syntax error see SIM command for details
+} 
+C = polyfit(Vm,im,2)
+{Undefined function or variable 'im'.
+} 
+C = polyfit(Vm,Im,2)
+{Undefined function or variable 'Im'.
+} 
+Im = [1.1 2.2 2.1 3.2 4.7]*le -3;
+{Error using <a href="matlab:matlab.internal.language.introspective.errorDocCallback('le')" style="font-weight:bold"> <= </a>
+Not enough input arguments.
+} 
+%saformesim vektoru x ar sikaku soli
+V = -1:0.01:3.2;
+%aprekinasim y
+I = C(1)*V^2+C(2)*V+C(3)
+{Undefined function or variable 'C'.
+} 
+I = C(1)*V.^2+C(2)*V+C(3)
+{Undefined function or variable 'C'.
+} 
+I = C(1)*V.^2+C(2)*V+C(3);
+{Undefined function or variable 'C'.
+} 
+Im = [1.1 2.2 2.1 3.2 4.7]*1e-3;
+C = polyfit(Vm,Im,2)
+
+C =
+
+    0.0002    0.0004    0.0015
+
+I = C(1)*V.^2+C(2)*V+C(3);
+
+plot(Vm,Im,'o',V,I)
+%atkartosim to pasu prieks 3 kartas polinomu
+C = polyfit(Vm,Im,3)
+
+C =
+
+    0.0002   -0.0004    0.0003    0.0021
+
+I = C(1)*V.^2+C(2)*V+C(3);
+I = C(1)*V.^3+C(2)*V+C(3);
+plot(Vm,Im,'o',V,I)
+I = C(1)*V.^2+C(3)*V+C(3);
+plot(Vm,Im,'o',V,I)
+plot(Vm,Im,'o',V,I)u
+ plot(Vm,Im,'o',V,I)u
+                    ↑
+{Error: Invalid expression. Check for missing multiplication operator, missing or unbalanced delimiters, or other syntax error. To
+construct matrices, use brackets instead of parentheses.
+} 
+plot(Vm,Im,'o',V,I)*u
+{Undefined function or variable 'u'.
+} 
+doty
+{Undefined function or variable 'doty'.
+} 
+dot 
+{Not enough input arguments.
+
+Error in <a href="matlab:matlab.internal.language.introspective.errorDocCallback('dot', '/usr/local/MATLAB/R2018a/toolbox/matlab/specfun/dot.m', 21)" style="font-weight:bold">dot</a> (<a href="matlab: opentoline('/usr/local/MATLAB/R2018a/toolbox/matlab/specfun/dot.m',21,0)">line 21</a>)
+if isinteger(a) || isinteger(b)
+} 
+ 
+plot(Vm,Im,'o',V,I)
+plot(Vm,Im,'o',V,I)
+dotgk(oybo rtoy
+ dotgk(oybo rtoy
+            ↑
+{Error: Invalid expression. Check for missing multiplication operator, missing or unbalanced delimiters, or other syntax error. To
+construct matrices, use brackets instead of parentheses.
+} 
+dot
+{Not enough input arguments.
+
+Error in <a href="matlab:matlab.internal.language.introspective.errorDocCallback('dot', '/usr/local/MATLAB/R2018a/toolbox/matlab/specfun/dot.m', 21)" style="font-weight:bold">dot</a> (<a href="matlab: opentoline('/usr/local/MATLAB/R2018a/toolbox/matlab/specfun/dot.m',21,0)">line 21</a>)
+if isinteger(a) || isinteger(b)
+} 
+dot
+{Not enough input arguments.
+
+Error in <a href="matlab:matlab.internal.language.introspective.errorDocCallback('dot', '/usr/local/MATLAB/R2018a/toolbox/matlab/specfun/dot.m', 21)" style="font-weight:bold">dot</a> (<a href="matlab: opentoline('/usr/local/MATLAB/R2018a/toolbox/matlab/specfun/dot.m',21,0)">line 21</a>)
+if isinteger(a) || isinteger(b)
+} 
+dot
+{Not enough input arguments.
+
+Error in <a href="matlab:matlab.internal.language.introspective.errorDocCallback('dot', '/usr/local/MATLAB/R2018a/toolbox/matlab/specfun/dot.m', 21)" style="font-weight:bold">dot</a> (<a href="matlab: opentoline('/usr/local/MATLAB/R2018a/toolbox/matlab/specfun/dot.m',21,0)">line 21</a>)
+if isinteger(a) || isinteger(b)
+} 
+dotgk(oybo rtoy
+ dotgk(oybo rtoy
+            ↑
+{Error: Invalid expression. Check for missing multiplication operator, missing or unbalanced delimiters, or other syntax error. To
+construct matrices, use brackets instead of parentheses.
+} 
+C = polyfit(Vm,Im,2)
+
+C =
+
+    0.0002    0.0004    0.0015
+
+C = polyfit(Vm,Im,2)
+
+C =
+
+    0.0002    0.0004    0.0015
+
+C = polyfit(Vm,Im,2)
+
+C =
+
+    0.0002    0.0004    0.0015
+
+C = polyfit(Vm,Im,2)
+
+C =
+
+    0.0002    0.0004    0.0015
+
+C = polyfit(Vm,Im,2)
+
+C =
+
+    0.0002    0.0004    0.0015
+
+C = polyfit(Vm,Im,2)
+
+C =
+
+    0.0002    0.0004    0.0015
+
+C = polyfit(Vm,Im,2)
+
+C =
+
+    0.0002    0.0004    0.0015
+
+C = polyfit(Vm,Im,2)
+
+C =
+
+    0.0002    0.0004    0.0015
+
+gh
+{Undefined function or variable 'gh'.
+} 
+gh
+{Undefined function or variable 'gh'.
+} 
+C = polyfit(Vm,Im,3)
+
+C =
+
+    0.0002   -0.0004    0.0003    0.0021
+
+I = C(1)*V.^2+C(2)*V+C(3);
+plot(Vm,Im,'o',V,I)
+I = C(1)*V.^3+C(4)*V+C(3);
+I = C(1)*V.^3+C(4)*V.^2+C(3);
+plot(Vm,Im,'o',V,I)
+plot(Vm,Im,'o',V,I)
+I = C(1)*V.^3+C(4)*V.^2+C(3)+c(4);
+{Undefined function or variable 'c'.
+} 
+I = C(1)*V.^3+C(4)*V.^2+C(3)+C(4);
+plot(Vm,Im,'o',V,I)
+plot(Vm,Im,'o',V,I)
+I = C(1)*V.^3+C(2)*V.^2+C(3)*V+C(4);
+plot(Vm,Im,'o',V,I)
+I = C(1)*V.^3+C(2)*V.^2+C(3)*V+C(4);
+%lai nerakstitu I = C(1)*V.^3+C(3)*V.^2+C(4)
+%izmanyosim funkciju polyval - polinomial
+%sintakse
+%y = polyval(C,x)
+I = polyval(C,V);
+plot(Vm,Im,'o',V,I)
+%atkartot 4,kartai
+%atkartot 1.kartai
+I = polyval(C,V);
+plot (Vm,Im,'o',V,I)
+C = polyfit(Vm,Im,4)
+
+C =
+
+   -0.0001    0.0004   -0.0006    0.0000    0.0022
+
+I = polyval(C,V);
+C = polyfit(Vm,Im,4)
+
+C =
+
+   -0.0001    0.0004   -0.0006    0.0000    0.0022
+
+plot (Vm,Im,'o',V,I)
+C = polyfit(Vm,Im,4)
+
+C =
+
+   -0.0001    0.0004   -0.0006    0.0000    0.0022
+
+C = polyfit(Vm,Im,1)
+
+C =
+
+    0.0007    0.0017
+
+I = polyval(C,V);
+plot (Vm,Im,'o',V,I)
+%merijumu serijas
+Vm = [-1 0.3 1.5 2.5 3.2];
+Im = [1.1 2.2 2.1 3.2 4.7
+         0.9 1.8 2.6 3.34.5
+          0.9 1.8 2.6 3.34.5
+                          ↑
+{Error: Invalid expression. Check for missing multiplication operator, missing or unbalanced delimiters, or other syntax error. To
+construct matrices, use brackets instead of parentheses.
+} 
+Im = [1.1 2.2 2.1 3.2 4.7
+0.9 1.8 2.6 3.3 4.5
+1.0 2.0 2.4 3.4 4.3 0,8 1,9 2,5 3,5 4,6
+Im = [1.1 2.2 2.1 3.2 4.7
+ Im = [1.1 2.2 2.1 3.2 4.7
+    ↑
+{Error: Incorrect use of '=' operator. To assign a value to a variable, use '='. To compare values for equality, use '=='.
+} 
+Im = [1.1 2.2 2.1 3.2 4.7
+         0.9 1.8 2.6 3.3 4.5
+         1.0 2.0 2.4 3.4 4.3
+         0.8 1.9 2.5 3.5 4.6
+         0.9 2.0 2.3 3.1 4.4];
+figure
+plot(Vm,Im,'o-')
+plot(Vm,Im','o-')
+sum([1 2 3 4 5])
+
+ans =
+
+    15
+
+sum9[1 2;3 4])
+ sum9[1 2;3 4])
+     ↑
+{Error: Invalid expression. When calling a function or indexing a variable, use parentheses. Otherwise, check for mismatched
+delimiters.
+} 
+Ivid = sum (Im)/5;
+Invid = mean(Im)
+
+Invid =
+
+    0.9400    1.9800    2.3800    3.3000    4.5000
+
+%videjai vertibai pielaikosim polinomu
+C = polyfit(Vm,Ivid,4);
+V = -1:0.01:3.2;
+I = polyval(C,V);
+%uzzimet 3 grafikus vienas asis
+%Vm,Im ar 'o'
+%Vm,Ivid ar'*'
+%V,I ar '-'
+plot(Vm,Im,'o',Vm,Ivid,'*')
+plot(Vm,Im,'o',Vm,Ivid,'*',V,I,'-');
+plot(Vm,Im','o',Vm,Ivid,'*',V,I,'-');
+Istd = std(Im)
+
+Istd =
+
+    0.1140    0.1483    0.1924    0.1581    0.1581
+
+errorbar(Vm,Im,Isdt)
+{Undefined function or variable 'Isdt'.
+} 
+errorbar(Vm,Ivid,Isdt)
+{Undefined function or variable 'Isdt'.
+} 
+errorbar(Vm,Ivid,Istd)errorbar(Vm,Ivid,Istd,'.')
+ errorbar(Vm,Ivid,Istd)errorbar(Vm,Ivid,Istd,'.')
+                       ↑
+{Error: Invalid expression. Check for missing multiplication operator, missing or unbalanced delimiters, or other syntax error. To
+construct matrices, use brackets instead of parentheses.
+} 
+errorbar(Vm,Ivid,Istd)*errorbar(Vm,Ivid,Istd,'.')
+{Undefined operator '*' for input arguments of type 'matlab.graphics.chart.primitive.ErrorBar'.
+} 
+hold on
+plot(V,I)
+hold off
+ls
+bilde1.png  bilde2.png	lab1_diary.m
+
+A = imread('bilde1.png');
+B = imread('bilde2.png');
+figure(1),image(A)
+figure(2),image(B)
+figure(2),image([500,800],[0,1],B)
+set(gca,'YDir','normal')
+figure(2),image([500,800],[1,0],B)
+set(gca,'YDir','normal')
+[x,y] = ginput(1)
+
+x =
+
+  449.2233
+
+
+y =
+
+   -0.1393
+
+[x,y] = ginput(10);
+hold on
+plot(x,y,'o')
+hold off
+diary off
